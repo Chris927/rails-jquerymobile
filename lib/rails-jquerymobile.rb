1174202ca4d61ae3879938ecc00e2f9d50bf4106
Chris Oloff <christian.oloff+github@googlemail.com>
2011-12-21 08:31:43 +0200

    Added debug option, cleaned up log output

--- lib/rails-jquerymobile.rb
+++ lib/rails-jquerymobile.rb
@@ -12,7 +12,6 @@ ActionController::Base.send :include, HasMobileFormat
 class RailsJquerymobile::LayoutGenerator < Rails::Generators::Base
   source_root File.expand_path("../templates", __FILE__)
   def create_mobile_layout
-    #create_file "app/views/layouts/application.mobile.erb", "<!-- This may be a layout... -->"
     copy_file "application.mobile.erb", "app/views/layouts/application.mobile.erb"
   end
 end
--- lib/rails-jquerymobile/has_mobile_format.rb
+++ lib/rails-jquerymobile/has_mobile_format.rb
@@ -1,4 +1,5 @@
 module HasMobileFormat
+
   def self.included(base)
     base.send :extend, ClassMethods
   end
@@ -6,26 +7,37 @@ module HasMobileFormat
   module ClassMethods
     def has_mobile_format(options = {})
       send :include, InstanceMethods
+      class_attribute :jquerymobile_options
+      self.jquerymobile_options = options
       before_filter :prepare_for_mobile_formats
     end
   end
 
   module InstanceMethods
     def mobile?
+      raise "not implemented yet"
     end
     # TODO: following should be private?
     def prepare_for_mobile_formats
       remember_format(params[:mobile].to_s)
-      puts "before assigning: request.format=#{request.format}, session[:remembered_format]=#{session[:remembered_format]}"
+      puts "before assigning: request.format=#{request.format}, " +
+           "session[:remembered_format]=#{session[:remembered_format]}" if debug?
       format = session[:remembered_format]
-      puts "format=#{format}"
+      puts "determined format=#{format}" if debug?
       request.format = format unless format.blank?
-      puts "request.format=#{request.format}"
+      puts "request.format=#{request.format}" if debug?
     end
+
+    private
+
     def remember_format(format)
       session[:remembered_format] = :mobile if format == '1'
       session[:remembered_format] = nil if format == '0'
-      puts "session[:remembered_format]=#{session[:remembered_format]}"
+      puts "session[:remembered_format]=#{session[:remembered_format]}" if debug?
+    end
+
+    def debug?
+      self.class.jquerymobile_options[:debug]
     end
   end
 
